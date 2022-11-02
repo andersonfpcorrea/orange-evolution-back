986dbeafebee1fc844c3c070f2c426fa1d01dd13
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-02 16:09:48 -0400

    refactor: model user

--- src/models/user.js
+++ src/models/user.js
@@ -1,39 +1,32 @@
-'use strict';
-
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init(
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
-      password: DataTypes.STRING,
+  const Users = sequelize.define("Users", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
     },
-    {
-      sequelize,
-      modelName: 'User',
-      tableName: 'users',
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
     }
-  );
-  return User;
+  }, { 
+    tableName: 'users',
+    timestamps: false 
+  });
+
+  return Users;
 };
