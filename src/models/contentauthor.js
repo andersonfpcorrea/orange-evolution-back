a8b16e0400443627e1d70fbd076ba2d8ffba14f4
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-02 16:00:28 -0400

    refactor: refatoração model contentauthor

--- src/models/contentauthor.js
+++ src/models/contentauthor.js
@@ -1,35 +1,19 @@
-'use strict';
-
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class ContentAuthor extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  ContentAuthor.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      author: {
-        type: DataTypes.STRING,
-        unique: true,
-      },
+  const ContentAuthors = sequelize.define("ContentAuthors", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize,
-      modelName: 'ContentAuthor',
-      tableName: 'content_authors',
-    }
-  );
-  return ContentAuthor;
+    author: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  }, { 
+    tableName: 'content_authors',
+    timestamps: false 
+  });
+
+  return ContentAuthors;
 };
