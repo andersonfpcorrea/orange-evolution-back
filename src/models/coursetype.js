c27d5f1bb302e93a8130fea8e4d00e8bdd3fd502
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-02 16:05:05 -0400

    refactor: refatoração model coursetype

--- src/models/coursetype.js
+++ src/models/coursetype.js
@@ -1,33 +1,16 @@
-'use strict';
-
-const { Model } = require('sequelize');
-
 module.exports = (sequelize, DataTypes) => {
-  class CourseType extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  CourseType.init(
-    {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-      },
-      title: DataTypes.STRING,
+  const CourseTypes = sequelize.define("CourseTypes", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
     },
-    {
-      sequelize,
-      modelName: 'CourseType',
-      tableName: 'course_types',
-    }
-  );
-  return CourseType;
+    title: DataTypes.STRING,
+  }, { 
+    tableName: 'course_types',
+    timestamps: false 
+  });
+
+  return CourseTypes;
 };
