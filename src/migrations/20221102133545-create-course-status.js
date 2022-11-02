c5e7b92e6c680ac8a532d968e7ad72d69321fe86
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-02 15:56:16 -0400

    refactor: refatoração migration course-status

--- src/migrations/20221102133545-create-course-status.js
+++ src/migrations/20221102133545-create-course-status.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('course_statuses', {
+    await queryInterface.createTable('courses_status', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -24,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, _Sequelize) {
-    await queryInterface.dropTable('course_statuses');
+    await queryInterface.dropTable('courses_status');
   },
 };
