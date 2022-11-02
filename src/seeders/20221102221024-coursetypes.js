a1d6ebb97a5cec6bc157aae8e21ce47a40c9ae7e
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-02 18:43:16 -0400

    feat: adiciona seeder coursetypes

---
+++ src/seeders/20221102221024-coursetypes.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('course_types',
+    [
+      {
+        id: 1,
+        title: 'Apostila',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+      {
+        id: 2,
+        title: 'Artigo',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+      {
+        id: 3,
+        title: 'Curso',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+      {
+        id: 4,
+        title: 'Livro',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+      {
+        id: 5,
+        title: 'Vídeo',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+      {
+        id: 6,
+        title: 'Live',
+        createdAt: '2022-11-02 15:00:30',
+        updatedAt: '2022-11-02 16:00:30',
+      },
+    ], {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('course_types', null, {}),
+};
