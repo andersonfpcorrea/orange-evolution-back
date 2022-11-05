600e6db78a8e8e80da09cd82e2dd9b78ba74c4e9
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-05 12:12:19 -0400

    feat: adiciona controller course

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const authController = require('./authController');
 const globalErrorHandler = require('./errorController');
 const userController = require('./userController');
 const roadmapController = require('./roadmapController');
+const courseController = require('./courseController');
 
 module.exports = {
   authController,
   globalErrorHandler,
   userController,
   roadmapController,
+  courseController,
 };
