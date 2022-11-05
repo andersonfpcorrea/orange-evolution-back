46c78056dfb9827b05d71f31b3c7910fb2b0f104
Kamila Pantoja <kms.pantoja@gmail.com>
2022-11-05 13:18:06 -0400

    test: adiciona teste da rota course

---
+++ __tests__/integration/courses.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../../src/app');
+const { coursesOutput } = require('../integration/outputs/coursesOutput');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+
+describe('Making a GET request on /courses', function() {
+  it('returns a list of all courses', async function() {
+    const response = await chai.request(app).get('/courses');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(coursesOutput);
+  })
+})
+
+describe('Making a GET request on /courses/1', function () {
+  it('returns a roadmap whose id is 1', async function() {
+    const response = await chai.request(app).get('/courses/1');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(coursesOutput[0]);
+  })
+})
