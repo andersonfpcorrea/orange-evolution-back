a9b83b0a6148880a1c1d4cb9d33178161d92c6c6
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-05 01:53:25 -0300

    feat: :sparkles: add progress route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./userRouter');
 const roadmapRouter = require('./roadmapRouter');
+const progressRouter = require('./progressRouter');
 
 module.exports = {
   userRouter,
-  roadmapRouter
+  roadmapRouter,
+  progressRouter,
 };
