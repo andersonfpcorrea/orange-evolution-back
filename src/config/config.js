22bf33e162f0abfcd96024aab508437662c80632
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-05 19:13:45 -0300

    fix: adjust config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,25 +1,16 @@
 // require('dotenv').config();
 
 const config = {
-  host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
-  port: process.env.MYSQLPORT || '3306',
-  database: process.env.MYSQLDATABASE || 'orangedb_dev',
-  username: process.env.MYSQLUSER || process.env.MYSQL_USER,
-  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
 };
 
 module.exports = {
-  development: {
-    ...config,
-  },
-  test: {
-    ...config,
-  },
-  dev: {
-    ...config,
-  },
+  development: config,
+  test: config,
+  production: config,
 };
