7645e04cc169b37b102c2acae766033d02123ba8
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-09 18:28:28 -0300

    feat: :sparkles: adjust progress seeders for development of frontend

--- __tests__/integration/progress.test.js
+++ __tests__/integration/progress.test.js
@@ -7,96 +7,69 @@ chai.use(chaiHttp);
 
 const output = [
   {
-    "idUser": 1,
-    "courses": [
-      "De advogada a desenvolvedora: um relato sobre minha migração de carreira",
-      "Guia definitivo de como migrar para UX Design: 5 passos para virar um UX",
-      "Design Thinking e carreira: como migrei de Psicologia para UX Design"
+    idUser: 1,
+    courses: [
+      'Habilidades além do código',
+      'Qual a melhor linguagem de programação?',
+      'LGPD: Desvendando o bicho de 7 cabeças',
+      'Design Thinking e carreira: como migrei de Psicologia para UX Design',
+      'Guia definitivo de como migrar para UX Design: 5 passos para virar um UX',
+      'O que um Dev júnior pode ensinar?',
+      'Mini Curso Javascript do Zero',
+      'O design do dia a dia',
+      'A poderosa união entre design de UX e desenvolvimento de software',
+      'De advogada a desenvolvedora: um relato sobre minha migração de carreira',
     ],
-    "status": [
-      "Não comecei",
-      "Não comecei",
-      "Em andamento"
+    status: [
+      'Em andamento',
+      'Em andamento',
+      'Em andamento',
+      'Em andamento',
+      'Em andamento',
+      'Concluído',
+      'Concluído',
+      'Concluído',
+      'Concluído',
+      'Concluído',
     ],
-    "favorites": [
-      1,
-      1,
-      0
-    ]
+    favorites: [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
   },
   {
-    "idUser": 2,
-    "courses": [
-      "Culture Code",
-      "Design Thinking e carreira: como migrei de Psicologia para UX Design"
+    idUser: 2,
+    courses: [
+      'Culture Code',
+      'Design Thinking e carreira: como migrei de Psicologia para UX Design',
     ],
-    "status": [
-      "Não comecei",
-      "Em andamento"
-    ],
-    "favorites": [
-      1,
-      1
-    ]
+    status: ['Em andamento', 'Em andamento'],
+    favorites: [1, 1],
   },
   {
-    "idUser": 3,
-    "courses": [
-      "De advogada a desenvolvedora: um relato sobre minha migração de carreira",
-      "Design Thinking e carreira: como migrei de Psicologia para UX Design",
-      "Guia definitivo de como migrar para UX Design: 5 passos para virar um UX",
-      "Culture Code"
+    idUser: 3,
+    courses: [
+      'Culture Code',
+      'Design Thinking e carreira: como migrei de Psicologia para UX Design',
+      'De advogada a desenvolvedora: um relato sobre minha migração de carreira',
+      'Guia definitivo de como migrar para UX Design: 5 passos para virar um UX',
     ],
-    "status": [
-      "Não comecei",
-      "Não comecei",
-      "Não comecei",
-      "Em andamento"
-    ],
-    "favorites": [
-      0,
-      1,
-      1,
-      1
-    ]
-  }
-]
+    status: ['Em andamento', 'Em andamento', 'Concluído', 'Concluído'],
+    favorites: [1, 1, 0, 1],
+  },
+];
 
-const user1 = [
-  {
-    "idUser": 1,
-    "courses": [
-      "Guia definitivo de como migrar para UX Design: 5 passos para virar um UX",
-      "Design Thinking e carreira: como migrei de Psicologia para UX Design",
-      "De advogada a desenvolvedora: um relato sobre minha migração de carreira"
-    ],
-    "status": [
-      "Não comecei",
-      "Em andamento",
-      "Não comecei"
-    ],
-    "favorites": [
-      1,
-      0,
-      1
-    ]
-  }
-]
-
-describe('Making a GET request on /progress', function() {
-  it("returns a list of all students' progress", async function() {
+describe('Making a GET request on /progress', function () {
+  it("returns a list of all students' progress", async function () {
     const response = await chai.request(app).get('/progress');
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.deep.equal(output);
-  })
-})
+  });
+});
 
 describe('Making a GET request on /progress/1', function () {
-  it('returns the progress of the student whose id is 1', async function() {
+  it('returns the progress of the student whose id is 1', async function () {
     const response = await chai.request(app).get('/progress/1');
 
     expect(response.status).to.be.equal(200);
-    expect(response.body).to.deep.equal(user1);
-  })
-})
+    expect(response.body).to.deep.equal([output[0]]);
+  });
+});
