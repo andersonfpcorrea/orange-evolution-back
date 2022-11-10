a7a90f5fd70316ccbac4c3b84d877eade9cd9629
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-10 14:36:53 -0300

    feat: :sparkles: add course types information to progress route response

--- src/services/progressService.js
+++ src/services/progressService.js
@@ -3,12 +3,13 @@ const { sequelize } = require('../models');
 
 exports.getOne = async (id) =>
   sequelize.query(
-    `SELECT ucs.idUser, JSON_ARRAYAGG(c.nameCourse) AS courses, JSON_ARRAYAGG(rm.title) AS roadmaps, JSON_ARRAYAGG(cs.status) AS status, JSON_ARRAYAGG(ucs.favoriteCourse) AS favorites
+    `SELECT ucs.idUser, JSON_ARRAYAGG(c.nameCourse) AS courses, JSON_ARRAYAGG(ct.title) AS courseTypes, JSON_ARRAYAGG(rm.title) AS roadmaps, JSON_ARRAYAGG(cs.status) AS status, JSON_ARRAYAGG(ucs.favoriteCourse) AS favorites
     FROM users_courses_status ucs
     INNER JOIN courses c
-    JOIN courses_status cs
+    INNER JOIN courses_status cs
     INNER JOIN roadmaps rm
-    WHERE ucs.idCourse = c.id AND ucs.idStatus = cs.id AND ucs.idUser = ? AND rm.id = c.roadmap
+    INNER JOIN course_types ct
+    WHERE ucs.idCourse = c.id AND ucs.idStatus = cs.id AND ucs.idUser = ? AND rm.id = c.roadmap AND ct.id = c.type
     GROUP BY ucs.idUser;`,
     {
       replacements: [id],
@@ -18,12 +19,13 @@ exports.getOne = async (id) =>
 
 exports.getAll = async () =>
   sequelize.query(
-    `SELECT ucs.idUser, JSON_ARRAYAGG(c.nameCourse) AS courses, JSON_ARRAYAGG(rm.title) AS roadmaps, JSON_ARRAYAGG(cs.status) AS status, JSON_ARRAYAGG(ucs.favoriteCourse) AS favorites
+    `SELECT ucs.idUser, JSON_ARRAYAGG(c.nameCourse) AS courses, JSON_ARRAYAGG(ct.title) AS courseTypes, JSON_ARRAYAGG(rm.title) AS roadmaps, JSON_ARRAYAGG(cs.status) AS status, JSON_ARRAYAGG(ucs.favoriteCourse) AS favorites
     FROM users_courses_status ucs
     INNER JOIN courses c
-    JOIN courses_status cs
+    INNER JOIN courses_status cs
     INNER JOIN roadmaps rm
-    WHERE ucs.idCourse = c.id AND ucs.idStatus = cs.id AND rm.id = c.roadmap
+    INNER JOIN course_types ct
+    WHERE ucs.idCourse = c.id AND ucs.idStatus = cs.id AND rm.id = c.roadmap AND ct.id = c.type
     GROUP BY ucs.idUser;`,
     { type: QueryTypes.SELECT }
   );
